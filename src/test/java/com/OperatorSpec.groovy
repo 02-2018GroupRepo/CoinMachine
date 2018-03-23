02ff2ed141fb32b3645bcebdf9102aa4b5a80e3e
syntel <syntel@PHX440G3-281611.syntelorg.com>
2018-03-23 11:46:55 -0400

    Add test for returning coins, adding nickels, adding dimes, and adding quarters

--- src/test/java/com/OperatorSpec.groovy
+++ src/test/java/com/OperatorSpec.groovy
@@ -6,17 +6,71 @@ import spock.lang.Specification
 class OperatorSpec extends Specification {
 
     def "Ask a vending machine the for the total money in machine"(){
-        given: "A vending machine"
+        given: "A vending machine pre-loaded with 2.35 dollars"
 
-        AbstractMachine vMachine = new SnackMachine();
-        vMachine.addNickels(2)
-        vMachine.addDimes(4)
-        vMachine.addQuarters(4)
+        AbstractMachine vMachine = new DrinkMachine();
 
         when: "The operator calls getMoneyAmount()"
-        double total = vMachine.getMoneyAmount();
+        String total = vMachine.getMoneyAmount();
 
         then:"The machine should return total amount of money inside the machine"
-        total == 1.50;
+        total == "2.35";
     }
+
+    def "Add quarters to a vending machine to increase the total amount"(){
+        given: "A vending machine pre-loaded with 2.35 dollars"
+        AbstractMachine vMachine = new SnackMachine();
+
+        when: "The operator wants to add 4 Quarters"
+        vMachine.addQuarters(4)
+        and: "Get the total amount of money back in the machine"
+        String total = vMachine.getMoneyAmount();
+
+        then:"The machine should return total amount of money inside the machine increased by 4 quarters (1.00)"
+        total == "3.35"
+    }
+
+    def "Add dimes to a vending machine to increase the total amount"(){
+        given: "A vending machine pre-loaded with 2.35 dollars"
+        AbstractMachine vMachine = new SnackMachine();
+
+        when: "The operator wants to add 3 Dimes"
+        vMachine.addDimes(3)
+        and: "Get the total amount of money back in the machine"
+        String total = vMachine.getMoneyAmount();
+
+        then:"The machine should return total amount of money inside the machine increased by 3 dimes (0.30)"
+        total == "2.65"
+    }
+
+    def "Add nickels to a vending machine to increase total amount"(){
+        given: "A vending machine pre-loaded with 2.35 dollars"
+        AbstractMachine vMachine = new SnackMachine();
+
+        when: "The operator wants to add 2 nickels"
+        vMachine.addNickels(2)
+        and: "Get the total amount of money back in the machine"
+        String total = vMachine.getMoneyAmount();
+
+        then:"The machine should return total amount of money inside the machine increased by 2 nickels (0.10)"
+        total == "2.45";
+    }
+
+
+    //get total coins per machine
+    def "Get a map of each coin in the machine"(){
+        given: "A vending machine pre-loaded with 2.35 dollars"
+        AbstractMachine vMachine = new SnackMachine();
+
+        when: "The operator wants to see the amount of each coin in the machine"
+        Map coins = vMachine.getCoinAmount();
+
+        then: "The machine should return a map of the coins and their amount"
+        coins.get(AbstractMachine.COINS.NICKEL) == 5;
+        coins.get(AbstractMachine.COINS.DIME) == 6;
+        coins.get(AbstractMachine.COINS.QUARTER) == 6;
+    }
+
+    //change location test
+    
 }
