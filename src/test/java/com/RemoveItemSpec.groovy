445943887faca81c05f3b1b6afb4081777b13d01
newsha400 <newsha400@gmail.com>
2018-03-23 11:25:55 -0400

    added more scenarios to addItem and removeItem

--- src/test/java/com/RemoveItemSpec.groovy
+++ src/test/java/com/RemoveItemSpec.groovy
@@ -31,34 +31,62 @@ class RemoveItemSpec extends Specification {
         vendingMachine.removeItem(rowColumn, runningTotal);
         then: "Number of products in that compartment decreases by one"
         thrown(BADENTRY);
-    }
 
-    def"removing an item from vending machine when running total is not enough for purchase"() {
+
+    }
+    def"removing an item from vending machine when the compartment code is entered incorrectly"() {
         given: "a machine"
         AbstractMachine vendingMachine = new SnackMachine();
-        and: "a compartment which consist of row and column name"
-        String rowColumn = "C4";
+        and: "an empty compartment, which consist of row and column name"
+        String rowColumn = "F2";
         and: "initial running total"
-        double runningTotal = 0.05;
+        double runningTotal = 1.00;
         when: "when the customer purchases the item"
         vendingMachine.removeItem(rowColumn, runningTotal);
         then: "Number of products in that compartment decreases by one"
-        thrown(INSUFFICIENTFUNDS);
-    }
-
-    def"Adding an item to the vending machine"() {
-        given: "a machine"
-        AbstractMachine vendingMachine = new SnackMachine();
-        and: "a compartment which consist of row and column name"
-        String rowColumn = "C2";
-        and: "a product"
-        Product snack = new Product("chips","original",0.5,1.0);
-        when: "when the customer purchases the item"
-        vendingMachine.addItem(rowColumn,snack);
-        then: "Appends the product to the end of that compartment"
-        vendingMachine.machine[2][2].getLast()==snack;
+        thrown(BADENTRY);
 
     }
+        def "removing an item from vending machine when running total is not enough for purchase"() {
+            given: "a machine"
+            AbstractMachine vendingMachine = new SnackMachine();
+            and: "a compartment which consist of row and column name"
+            String rowColumn = "C4";
+            and: "initial running total"
+            double runningTotal = 0.05;
+            when: "when the customer purchases the item"
+            vendingMachine.removeItem(rowColumn, runningTotal);
+            then: "Number of products in that compartment decreases by one"
+            thrown(INSUFFICIENTFUNDS);
+        }
+
+        def "Adding an item to the vending machine"() {
+            given: "a machine"
+            AbstractMachine vendingMachine = new SnackMachine();
+            and: "a compartment which consist of row and column name"
+            String rowColumn = "C2";
+            and: "a product"
+            Product snack = new Product("chips", "original", 0.5, 1.0);
+            when: "when the customer purchases the item"
+            vendingMachine.addItem(rowColumn, snack);
+            then: "Appends the product to the end of that compartment"
+            vendingMachine.machine[2][2].getLast() == snack;
+
+        }
+
+        def "Entering the incorrect compartment name when adding an item to the vending machine"() {
+            given: "a machine"
+            AbstractMachine vendingMachine = new SnackMachine();
+            and: "a compartment which consist of row and column name"
+            String rowColumn = "F2";
+            and: "a product"
+            Product snack = new Product("chips", "original", 0.5, 1.0);
+            when: "when the customer purchases the item"
+            vendingMachine.addItem(rowColumn, snack);
+            then: "Appends the product to the end of that compartment"
+            thrown(BADENTRY);
+
+
+        }
 
 }
-
